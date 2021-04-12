f4c60f014fffe5018170253a6e9c52172957ea85
Daniel Rodriguez <drodriguez661@gmail.com>
2021-04-11 23:17:24 -0500

    made a bundle file not in config but in terminal, if needing to produce more pages ill continue

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+/*! For license information please see bundle.js.LICENSE.txt */
+(()=>{var __webpack_modules__={"./src/index.js":()=>{eval("console.log(alert('hello from webpack'));\n\n//# sourceURL=webpack://portfolio/./src/index.js?")}},__webpack_exports__={};__webpack_modules__["./src/index.js"]()})();
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: ''
+  }, 
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024 // 3 kilobytes
+          }
+        }
+      },
+      {
+        test: /\.txt/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'style-loader', 'css-loader'
+        ]
+      },
+      // {
+      //   test: /\.scss$/,
+      //   use: [
+      //     MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+      //   ]
+      // },
+      {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [ '@babel/env' ],
+          plugins: [ '@babel/plugin-proposal-class-properties' ]
+        }
+      }
+      }
+    ]
+  },
+  plugins: [
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css'
+    }),
+    // use to clean dist with build.!
+    //new CleanWebpackPlugin({
+    //   cleanOnceBeforeBuildPatterns: [
+    //     '**/*',
+    //     path.join(process.cwd(), 
+    //'build/**/*')]}),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin()
+  ]
+};
